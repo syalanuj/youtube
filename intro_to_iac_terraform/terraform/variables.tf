3c8ce13076fae10be3f0763e5be4dd55ce37d35f
Anuj Syal <anuj@quilt.ai>
2022-07-12 12:44:53 +0800

    terraform infrastructure as code

---
+++ intro_to_iac_terraform/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "external-social-tracking"
+}
+
+variable "credentials_file" {
+  default = "key.json"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
